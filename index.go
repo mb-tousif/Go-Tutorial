c061f2f87184055323107d8a47a1860884a97cc0
mb-tousif <meta.block.tousif@gmail.com>
2024-07-13 10:56:15 +0600

    🚶🏻‍♀️

--- index.go
+++ index.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-var name string = "John Doe"
+var name string = "Gopher"
 
 func main() {
-	fmt.Println("Hello,", name)
+	fmt.Println("Hello, " + name)
+	printTime()
+}
+
+func printTime() {
+	t := time.Now()
+	// day-month-year hour:minute:second
+	fmt.Println(t.Format("02-01-2006 15:04:05 MST"))
 }
